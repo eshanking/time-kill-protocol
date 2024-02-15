626c7c2ac74455abecc85f91b1122f43e74c0dc9
eshanking <eshanking@gmail.com>
2024-02-15 13:40:38 -0500

    DNase analysis

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats
+import scipy.stats as stats
 import re
 import scipy.optimize as sciopt
 from scipy.integrate import odeint
@@ -61,7 +61,7 @@ def est_linear_slope(counts,
     for indx in range(exclude,len(lnCount)-window+step,step):
         time_t = time[indx:indx+window]
         subset = lnCount[indx:indx+window]
-        fit = scipy.stats.linregress(time_t,subset)
+        fit = stats.linregress(time_t,subset)
         slopes.append(fit.slope)
         x_pos.append(indx)
     
@@ -91,7 +91,7 @@ def est_linear_slope(counts,
     # compute slope and plot result
     time_t = time[lin_range]
     lin_seg = lnCount[lin_range]
-    fit = scipy.stats.linregress(time_t,lin_seg)
+    fit = stats.linregress(time_t,lin_seg)
     slope = fit.slope
 
     count_fit = time_t*fit.slope + fit.intercept
@@ -271,3 +271,67 @@ def get_timeseries(folder_path,plate_num=None):
             data.append(p.od_data_to_dict(p.data))
             
     return plates,data
+
+def linear_range_obj_fn(X,Y,l,k,slope_ub=np.inf,slope_lb=-np.inf):
+    """Objective function for finding the optimal linear range
+
+    Args:
+        X (array-like): independent variable
+        Y (array-like): dependent variable
+        l (float): regularization parameter
+    """
+
+    # estimate linear regression
+
+    res = stats.linregress(X,Y)
+
+    # if slope_ub is not None:
+    if res.slope > slope_ub:
+        return np.inf
+    
+    if res.slope < slope_lb:
+        return np.inf
+    
+    else:
+        n = len(X)
+
+        return (k*(res.stderr+1)**2 + l/(n**0.5))
+
+def opt_linear_range(X,Y,l,k,slope_ub=np.inf,slope_lb=-np.inf):
+    """Finds the optimal linear range for a given dataset
+
+    Args:
+        X (array-like): independent variable
+        Y (array-like): dependent variable
+        l (float): regularization parameter
+    """
+
+    if type(X) is list:
+        X = np.array(X)
+    if type(Y) is list:
+        Y = np.array(Y)
+    
+    # for each subset of X, compute the objective function
+    n = len(X)
+
+    loss_list = []
+    subset_list = []
+
+    # print('\n')
+    # print('n = ' + str(n))
+    
+    for subset_length in range(2,n):
+        for start in range(n-subset_length):
+            end = start + subset_length
+            X_subset = X[start:end]
+            Y_subset = Y[start:end]
+
+            # print(end)
+
+            loss = linear_range_obj_fn(X_subset,Y_subset,l,k,slope_ub=slope_ub,slope_lb=slope_lb)
+            loss_list.append(loss)
+            subset_list.append((start,end))
+    
+    # find the subset with the minimum loss
+    min_loss_indx = np.argmin(loss_list)
+    return subset_list[min_loss_indx]
